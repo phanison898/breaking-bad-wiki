a3910bf06b1e21aab6f579e22169165fafeab3ff
phanison898 <phanison898@gmail.com>
2021-03-31 23:22:47 +0530

    added backgroung music

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style.css";
+import VolumeUpIcon from "@material-ui/icons/VolumeUp";
+import VolumeOffIcon from "@material-ui/icons/VolumeOff";
+import bb_music from "../assets/bb_music.mp3";
 
 const Header = () => {
+  const [mute, setMute] = useState(false);
+
   return (
     <div className="header">
-      <h1>Breaking Bad Wiki</h1>
+      <div className="heading">Breaking Bad Wiki</div>
+      <audio loop autoPlay muted={mute}>
+        <source src={bb_music} type="audio/mp3" />
+      </audio>
+      <div className="nav-bar">
+        {mute ? (
+          <VolumeOffIcon onClick={() => setMute(false)} />
+        ) : (
+          <VolumeUpIcon onClick={() => setMute(true)} />
+        )}
+      </div>
     </div>
   );
 };
