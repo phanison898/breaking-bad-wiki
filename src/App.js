057882a05f8a51ebabfb0002fab5bcb5d432c57f
phanison898 <phanison898@gmail.com>
2021-03-31 20:35:10 +0530

    added UI skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Header, Body, Footer } from "./components";
 
 const App = () => {
-  return <div>Breaking Bad Wiki</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <Card />
+    </div>
+  );
+};
+
+export default Body;
